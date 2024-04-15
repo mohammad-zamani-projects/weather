f6712a37053939825e6edc972adb096e1b482399
amir <m.zamani@hpds.ir>
2024-04-15 13:27:57 +0330

    and finally email has sended
    '

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from weather.utils import send_mail
 
 # check  https://www.geeksforgeeks.org/building-a-weather-cli-using-python/   <--It's useful
 if __name__ == "__main__":
-    # get_weather_json()
-    send_mail()
+    get_weather_json()
+    # send_mail()
 
 
 
--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,3 @@
-from getpass import getpass
-
 API_KEY = "6bb9f4b0070845534df9e57c3a56ae51"
 MAIN_URL = "http://api.openweathermap.org/data/2.5/weather?"
 LOCATION = "Tehran"
